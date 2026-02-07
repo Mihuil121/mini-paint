982674e4bc97ddaedcf4a774420acb67d8dd84a5
Mihuil121 <bezugliymicha@gmail.com>
2026-02-07 20:05:45 +0300

    feat: add MobX, implement OOP structure for brush and tool classes

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import '../styles/canvas.scss'
+import { observer } from 'mobx-react-lite'
+import canvasState from '../store/canvasState'
 
-const Canvas = () => {
+const Canvas = observer(() => {
+  const canvasRef = useRef()
+
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current)
+  }, [])
+
   return (
     <div className='canvas'>
-        <canvas width={1200} height={900}>
+      <canvas ref={canvasRef} width={1200} height={900}>
 
-        </canvas>
+      </canvas>
     </div>
   )
-}
+})
 
 export default Canvas
